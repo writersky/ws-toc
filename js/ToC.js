58a66fcda38cf18e04ae0625616c17cfa1dd584b
Harris Wong <harris.wong@gmail.com>
2012-10-09 11:35:46 -0600

    ws-toc: Move toc codes to component
    Pulled out the toc codes to its own component, can now be loaded with config and populate the sidemenu automatically.

---
+++ js/ToC.js
@@ -0,0 +1,54 @@
+Heartstring.ToC = Heartstring.ToC || {};
+
+/** 
+ * component code 
+ */
+Heartstring.ToC = function (container, config) {
+    var that = Heartstring.init(Heartstring.ToC, config);
+    
+    /** 
+     * Create table of content and map section
+     */
+    that.generateMenu = function() {
+        var sections = Heartstring.select(that.config.selectors.articles).find('section');
+        $.each(sections, function(index, e) {
+            var section = $(e);  //turn <section> to a jQuery object
+            //use index to generate unique class name to map ToC
+            var bookmarkName = that.config.selectors.bookmarkPrefix + '-' + index;
+            var chapterName = section.find(':header').first().clone();
+            var tocList = Heartstring.select(that.config.selectors.navbar).find('.nav');
+            section.attr('id', bookmarkName);
+            tocList.append(
+                $('<li>').append(
+                    $('<a>').attr('href','#' + bookmarkName).append(chapterName)
+            ));
+        });
+    };
+     
+    return that;
+}
+
+/**
+ * Content init
+ */
+Heartstring.ToC.init = function(container, config) {    
+    // create this component
+    var that = Heartstring.ToC(container, config);
+    that.generateMenu();
+};
+
+Heartstring.ToC.bind = function(that) {
+    that.bindRow(that);
+}
+
+//default settings for this component
+Heartstring.ToC.defaults = {
+    container: ".hs-fontbar",
+    config: {
+        selectors: {
+            navbar: "ws-toc",  //the menu bar
+            articles: "ws-story",
+            bookmarkPrefix: "ws-toc"  //note, must be followed by a dash at the end.
+        }
+    }
+};
